231c6a48f526695421f4743221b5bd5c0bdc9b31
Patrick Miller <patrickmiller@mini.local>
2020-07-02 15:42:07 -0400

    cleanup

--- propertyinspector/js/kef.js
+++ propertyinspector/js/kef.js
@@ -3,7 +3,6 @@ $(function () {
     let uuid = null;
     let actionInfo = {};
     let setConnectionState = function(connected=true){
-        console.log("in setconnection state", connected)
         if(connected){
             $('.status').removeClass('red').addClass('green');
         }else{
@@ -19,24 +18,17 @@ $(function () {
         actionInfo = JSON.parse(inActionInfo); // cache the info
         websocket = new WebSocket('ws://localhost:' + inPort);
 
-        // refreshUI(actionInfo)
         websocket.onopen = function () {
             var json = {
                 event: inRegisterEvent,
                 uuid: inUUID
             };
 
-            // register property inspector to Stream Deck
             websocket.send(JSON.stringify(json));
-            // requestGlobalSettings(inUUID);
 
         }
 
-
-
-        // {"action":"com.patrickdmiller.kef.power","context":"F5F35CCBCCA21FF79BFC172F5CE3A542","event":"sendToPropertyInspector","payload":{"settings":{"clientAddress":"192.168.1.60","clientPort":"50001","maxVolume":50}}}
         websocket.onmessage = function (evt) {
-            console.log(evt);
             if (evt && evt.data) {
                 var jsonObj = JSON.parse(evt.data);
                 if (jsonObj && jsonObj.event == 'sendToPropertyInspector' && jsonObj.payload) {
@@ -70,8 +62,7 @@ $(function () {
                             e.value = jsonObj.payload.kef.muted;
                         }
                     } else if (jsonObj.payload.connect!==null){
-                    //connect state update
-                        console.log("IN HERE")
+
                         setConnectionState(jsonObj.payload.connect)
                         
                     
@@ -85,45 +76,7 @@ $(function () {
             }
         }
     }
-
-    function refreshUI(actionInfo) {
-
-        if (actionInfo && actionInfo.payload && actionInfo.payload.settings) {
-            if (actionInfo.payload.settings.hasOwnProperty('address')) {
-                const addressSelector = document.getElementById("Address");
-                const value = actionInfo.payload.settings.address;
-                addressSelector.value = value;
-            }
-
-            if (actionInfo.payload.settings.hasOwnProperty('value')) {
-                const valueSelector = document.getElementById("value");
-                const value = actionInfo.payload.settings.value;
-                valueSelector.value = value;
-            }
-
-            // console.log(actionInfo)
-            if (actionInfo.payload.settings.hasOwnProperty('clientAddress')) {
-                const clientAddressSelector = document.getElementById("clientAddress");
-                const value = actionInfo.payload.settings.clientAddress;
-                clientAddressSelector.value = value;
-            }
-
-            if (actionInfo.payload.settings.hasOwnProperty('clientPort')) {
-                const clientAddressSelector = document.getElementById("clientPort");
-                const value = actionInfo.payload.settings.clientPort;
-                clientAddressSelector.value = value;
-            }
-        }
-
-        // const addressSelector = document.getElementById("Address");
-        // const value = JSON.stringify(actionInfo.payload);
-        // addressSelector.value = value;
-
-    }
     $('.sdpi-item-value.user-defined').change(function (e) {
-        console.log(e)
-        console.log($(this))
-        console.log($(this).value, $(this).attr('data-param'))
         let value = $(this).val();
         let param = $(this).attr('data-param')
         if (websocket) {
@@ -138,7 +91,7 @@ $(function () {
 
                 }
             };
-            console.log(json);
+
             websocket.send(JSON.stringify(json));
         }
 
